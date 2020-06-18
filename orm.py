f15421277aae28b08b01ff18f1d6c32c0eab2732
Taras <ktaras88@gmail.com>
2020-06-18 15:03:14 +0300

    3

--- orm.py
+++ orm.py
@@ -4,9 +4,9 @@ class Database:
     
     def __init__(self, db_name):
         self.conn = sqlite3.connect(f'{db_name}.db')
+        self.c = self.conn.cursor()
 
     def create_table(self, table_name, **col_name_types):
-        self.c = self.conn.cursor()
         col_type = self.__format_string(**col_name_types)
         self.c.execute(f'''CREATE TABLE IF NOT EXISTS {table_name}(id INTEGER PRIMARY KEY AUTOINCREMENT, {col_type})''')
         self.conn.commit()
@@ -17,5 +17,5 @@ class Database:
             string += f'{item} {kwargs[item]}, '
         return string[0: -2]
 
-d = Database('testdb')
-d.create_table('test2', name1='TEXT', name2='INTEGER')
+d = Database('database_test')
+d.create_table('table_test', name1='TEXT', name2='INTEGER')
