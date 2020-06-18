58c31e9718be436e48b48fc5897a183c80e843e3
Taras <ktaras88@gmail.com>
2020-06-18 14:51:27 +0300

    //

--- orm.py
+++ orm.py
@@ -3,7 +3,7 @@ import sqlite3
 class Database:
     
     def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name +'.db')
+        self.conn = sqlite3.connect(f'{db_name}.db')
 
     def create_table(self, table_name, **col_name_types):
         self.c = self.conn.cursor()
@@ -17,6 +17,5 @@ class Database:
             string += f'{item} {kwargs[item]}, '
         return string[0: -2]
 
-d = Database()
-d.create_database('test1')
+d = Database('testdb')
 d.create_table('test2', name1='TEXT', name2='INTEGER')
